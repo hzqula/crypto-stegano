a012476bebf3877779e9c6cf60c345828dbfa667
hzqula <ilooqstrasi@gmail.com>
2024-10-06 00:54:25 +0700

    🎯 chore: ngubah style button

--- src/components/BtnPrimary.tsx
+++ src/components/BtnPrimary.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 
 interface BtnProps {
-  color?: string;
+  bgColor: string;
+  borderColor: string;
   label: string;
   onClick?: () => void;
 }
 
-const BtnPrimary: React.FC<BtnProps> = ({ color, label, onClick }) => {
+const BtnPrimary: React.FC<BtnProps> = ({
+  bgColor,
+  borderColor,
+  label,
+  onClick,
+}) => {
   return (
     <button
       onClick={onClick}
-      className={`inline-block py-2 px-4 transition-all font-text text-sm text-primary duration-300 relative text-center bg-${color}
+      className={`inline-block w-full py-2 px-4 transition-all font-text text-sm text-background font-semibold duration-300 relative text-center ${bgColor}
       hover:translate-x-[-2px] hover:translate-y-[-2px]
-      after:content-[''] after:absolute after:border-${color} after:-z-10 after:border-2 after:h-full after:w-full after:transition-all after:duration-300 after:top-0 after:left-0 
+      after:content-[''] after:absolute after:${borderColor} after:-z-10 after:border-2 after:h-full after:w-full after:transition-all after:duration-300 after:top-0 after:left-0 
       hover:after:translate-x-[4px] hover:after:translate-y-[4px]`}
     >
       {label}
