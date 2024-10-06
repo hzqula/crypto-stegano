d9b4ae22fb313dc9fcb126c3ac9cfe2c0c0d986f
hzqula <ilooqstrasi@gmail.com>
2024-10-06 08:49:22 +0700

    🔥 feat: nambahin router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Header from "./components/Header";
 import Steganography from "./pages/Steganography";
+import Landing from "./pages/Landing";
+import Cryptography from "./pages/Cryptography";
 
-function App() {
+const Layout = () => {
   return (
     <>
       <Header />
-      <Steganography />
+      <Outlet />
     </>
   );
-}
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, // Menggunakan Layout sebagai rute utama
+    children: [
+      {
+        path: "/", // Halaman Landing sebagai default
+        element: <Landing />,
+      },
+      {
+        path: "cryptography",
+        element: <Cryptography />,
+      },
+      {
+        path: "steganography",
+        element: <Steganography />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
