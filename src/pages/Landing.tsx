92ae18a7e21b2a6a08a1d57c8ee7f15d98500edf
hzqula <ilooqstrasi@gmail.com>
2024-10-06 08:50:54 +0700

    🔥 feat: layout awal

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="flex items-center justify-center h-[calc(100vh_-_16rem)] text-9xl">
+      <h1 className="font-bold text-primary font-code">Landing Page</h1>
+    </div>
+  );
+};
+
+export default Landing;
