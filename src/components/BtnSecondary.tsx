bf46d65e8cb1ee281b54c01ab7ac13fc99b1e2d8
hzqula <ilooqstrasi@gmail.com>
2024-10-10 10:49:07 +0700

    🎯 chore: ngubah style button

--- src/components/BtnSecondary.tsx
+++ src/components/BtnSecondary.tsx
@@ -16,7 +16,7 @@ const BtnSecondary: React.FC<BtnProps> = ({
   return (
     <button
       onClick={onClick}
-      className={`inline-block px-3 relative top-0 left-0 py-2 transition-all duration-100 border-2 ${borderColor} bg-background ${textColor}
+      className={`inline-block px-3 font-text text-sm font-semibold relative top-0 left-0 py-2 transition-all duration-100 border-2 ${borderColor} bg-background ${textColor}
               hover:top-[-2px] hover:left-[-2px]
               after:content-[''] after:absolute after:${borderColor} after:-z-10 after:border-2 after:h-full after:w-full after:transition-all after:duration-300 after:top-0 after:left-0 
               hover:after:translate-x-[6px] hover:after:translate-y-[6px]`}
