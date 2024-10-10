cc7f50eafa57149d6ee4531823228639f4491a25
hzqula <ilooqstrasi@gmail.com>
2024-10-10 10:49:41 +0700

    🎯 chore: nambahin style untuk link aktif

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,44 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="top-2 border-primary border-2 right-2 left-2 z-[40] items-center justify-around px-24 h-24 md:flex m-4 bg-transparent hidden">
       <div className="px-4 py-2">
         <h1 className="text-2xl font-bold text-primary font-code">
-          <a href="#">CryptoStegano</a>
+          <Link to={"/"}>CryptoStegano</Link>
         </h1>
       </div>
       <nav className="flex justify-between h-auto">
         <ul className="flex items-center justify-center gap-4 text-sm font-bold font-text">
           <li>
-            <a
-              className="inline-block py-2 px-4 transition-all duration-300 relative text-center bg-secBlue text-background
-              hover:translate-x-[-2px] hover:translate-y-[-2px]
-              after:content-[''] after:absolute after:border-secBlue after:-z-10 after:border-2 after:h-full after:w-full after:transition-all after:duration-300 after:top-0 after:left-0 
-              hover:after:translate-x-[4px] hover:after:translate-y-[4px]"
-              href="#"
+            <NavLink
+              to="/cryptography"
+              className={({ isActive }: { isActive: boolean }) =>
+                `inline-block py-2 px-4 transition-all duration-300 relative text-center bg-secBlue text-background
+      hover:translate-x-[-2px] hover:translate-y-[-2px]
+      after:content-[''] after:absolute after:border-secBlue after:-z-10 after:border-2 after:h-full after:w-full after:transition-all after:duration-300 after:top-0 after:left-0 
+      hover:after:translate-x-[4px] hover:after:translate-y-[4px] ${
+        isActive ? "bg-activeBlue text-white" : ""
+      }`
+              }
             >
               Cryptography
-            </a>
+            </NavLink>
           </li>
           <li className="relative">
-            <a
-              href="#"
-              className="inline-block px-3 relative top-0 left-0 py-2 transition-all duration-100 border-2 border-secPurple bg-background text-secPurple
+            <NavLink
+              to="/steganography"
+              className={({ isActive }: { isActive: boolean }) =>
+                `inline-block px-3 relative top-0 left-0 py-2 transition-all duration-100 border-2 border-secPurple bg-background text-secPurple
               hover:top-[-2px] hover:left-[-2px]
               after:content-[''] after:absolute after:border-secPurple after:-z-10 after:border-2 after:h-full after:w-full after:transition-all after:duration-300 after:top-0 after:left-0 
-              hover:after:translate-x-[6px] hover:after:translate-y-[6px]"
+              hover:after:translate-x-[6px] hover:after:translate-y-[6px] ${
+                isActive ? "bg-activeBlue text-white" : ""
+              }`
+              }
             >
               Steganography
-            </a>
+            </NavLink>
           </li>
         </ul>
       </nav>
