aad77b5ed81e6bc5992da9b5d659d229280a72e8
hzqula <ilooqstrasi@gmail.com>
2024-10-11 21:28:42 +0700

    🎯 chore: batalin useeffect

--- src/pages/Steganography.tsx
+++ src/pages/Steganography.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import BtnPrimary from "../components/BtnPrimary";
 
 const Steganography: React.FC = () => {
